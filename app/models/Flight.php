a334c8e76859e52470d490fd2a8839a9c3439c20
justinf111 <justin_f_111@hotmail.com>
2013-10-30 20:34:33 +1100

    Moved code from model to library

--- app/models/Flight.php
+++ app/models/Flight.php
@@ -5,14 +5,4 @@ class Flight extends Eloquent {
 
 	public static $rules = array();
 
-	public function api_call($function) {
-		$config = array(
-      'appId' => 'bf28d4a0',
-      'appKey' => 'a7ca1c2a0eab46e9d097f4aa39168ac7',
-    );
-		$client = new Client('https://api.flightstats.com/flex/flightstatus/rest/v2/json/');
-		$request = $client->get('airport/status/ABQ/dep/2013/10/27/12', $config);
-		$response = $request->send();
-		$data = json_decode (json_encode ($response->json()), FALSE);
-	}
 }
