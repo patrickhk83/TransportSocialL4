f5172ebd53519baecd066618fc8578599d9b7b26
justinf111 <justin_f_111@hotmail.com>
2013-10-31 21:17:15 +1100

    Removed none user routes and added some default user routes

--- app/routes/userRoutes.php
+++ app/routes/userRoutes.php
@@ -1,15 +1,10 @@
 <?php
 
-Route::get('/', function()
-{
-	return Redirect::route('posts.index');
-});
+Route::get('login',  array('as' => 'users.login', 'uses' => 'UsersController@login'));
+Route::get('logout',  array('as' => 'users.logout', 'uses' => 'UsersController@logout'));
 
-Route::get('login', 'UsersController@login');
-Route::get('flights/by_airport', 'FlightsController@by_airport');
-Route::get('flights/by_route', 'FlightsController@by_route');
-Route::get('flights/by_flight_num', 'FlightsController@by_flight_num');
+Route::get('user/{id}/flights', array('as' => 'user.flights', 'uses' => 'UserController@saved_flights'))
+		 ->where(array('id' => '[0-9]+'));
 
-Route::resource('flights', 'FlightsController');
-Route::resource('users', 'UsersController');
-Route::resource('posts', 'PostsController');
+Route::get('user/{id}/profile', array('as' => 'user.profile', 'uses' => 'UserController@profile'))
+		 ->where(array('id' => '[0-9]+'));
