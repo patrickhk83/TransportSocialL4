526ad29377cabe91c60063ca4c7456188522200c
justinf111 <justin_f_111@hotmail.com>
2013-10-31 21:16:48 +1100

    Changed the links to use named routes

--- app/views/_partials/header.blade.php
+++ app/views/_partials/header.blade.php
@@ -1,7 +1,29 @@
 @section("header")
-    <div class="header">
-        <div class="container">
-            <h1>Tutorial</h1>
-        </div>
-    </div>
+  <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    {{ link_to_route('site.home', 'Transport Social', null, array('class' => 'navbar-brand')) }}
+  </div>
+
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li>{{ link_to_route('flights.by_airport', 'By Airport') }}</li>
+      <li>{{ link_to_route('flights.by_route', 'By Route') }}</li>
+      <li>{{ link_to_route('flights.by_flight_num', 'By Flight') }}</li>
+      @if(!Auth::guest())
+        <li>{{ link_to_route('user.flights', 'Save Flights', array($user->id)) }}</li>
+        <li>{{ link_to_route('user.profile', 'My Profile', array($user->id)) }}</li>
+        <li>{{ link_to_route('users.logout', 'Logout') }}</li>
+      @else
+        <li>{{ link_to_route('users.login', 'Login') }}</li>
+      @endif
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
 @show
