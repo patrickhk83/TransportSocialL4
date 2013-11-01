227910639038cd02d063e80383049d3cf1fcc5e1
justinf111 <justin_f_111@hotmail.com>
2013-11-01 21:52:20 +1100

    Removed the unnecessary functions from controller and setup search from with results

--- app/controllers/FlightsController.php
+++ app/controllers/FlightsController.php
@@ -2,92 +2,37 @@
 
 class FlightsController extends BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$flightStatus = new Services\Flightstat\FlightStatus;
-		$flight = $flightStatus->find('313336893');
-		$data['flights'] = $flight->flightStatuses;
-		return View::make('flights.index', $data);
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-        return View::make('flights.create');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-        return View::make('flights.show');
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-        return View::make('flights.edit');
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
 	public function by_airport() {
+		if(Request::getMethod() == 'POST') {
+			$validation = new Services\Validators\FlightsByAirport;
+			return self::getFlightResults($validation, 'by_airport');
+		}
 		return View::make('flights.by_airport');
 	}
 
 	public function by_route() {
+		if(Request::getMethod() == 'POST') {
+			$validation = new Services\Validators\FlightsByAirport;
+			return self::getFlightResults($validation, 'by_route');
+		}
 		return View::make('flights.by_route');
 	}
 
 	public function by_flight_num() {
+		if(Request::getMethod() == 'POST') {
+			$validation = new Services\Validators\FlightsByAirport;
+			return self::getFlightResults($validation, 'by_flight_num');
+		}
 		return View::make('flights.by_flight_num');
+	}
+
+	public function getFlightResults($validation, $function_name) {
+		if($validation->passes()) {
+			$flightStatAPI = new Services\Flightstat\FlightStatus;
+			$flights = $flightStatAPI->{$function_name}(Input::all());
+			$data['flights'] = $flights->flightStatuses;
+			return View::make('flights.index', $data);
+		}
+		return Redirect::back()->withInput()->withErrors($validation->errors);
 	}
 }
