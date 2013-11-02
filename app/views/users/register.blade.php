1ec7fb34fb5e97759f72f54b577fcf4af434eaeb
ddksaku <ddksaku@gmail.com>
2013-11-02 13:46:34 +0800

    created a  view for the register form

---
+++ app/views/users/register.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts/default')
+@extends('layouts.default')
+@section('content')
+	<script src="{{ asset('js/manageuser.js') }}"></script>
+
+	<div class="page-header">
+		<h1>Signup Now For a Account</h1>
+	</div>	
+
+	@include('_partials.errors')
+	{{ Form::open(array('route' => 'users.register', 'class' => 'form-horizontal', 'method' => 'post', 'id' => 'customer_form'))}}
+		<input type="hidden" name="csrf_token" value="" />
+		<div class="form-group">
+			{{ Form::label('first_name' , trans('user_auth.create_user_fname_label'))}}
+			{{ Form::text('first_name', null, array('class' => 'form-control')) }}
+		</div> 	
+
+		<div class="form-group">
+			{{ Form::label('last_name' , trans('user_auth.create_user_lname_label'))}}
+			{{ Form::text('last_name', null, array('class' => 'form-control')) }}
+		</div>	
+
+		<div class="form-group">
+			{{ Form::label('email' , trans('user_auth.create_user_email_label'))}}
+			{{ Form::text('email', null, array('class' => 'form-control')) }}
+		</div>
+
+		<div class="form-group">
+			{{ Form::label('passwords' , trans('user_auth.create_user_password_label'))}}
+			{{ Form::password('passwords' , array('class' => 'form-control')) }}
+		</div>
+
+		<div class="form-group">
+			{{ Form::label('repeat_password' , trans('user_auth.create_user_password_confirm_label'))}}
+			{{ Form::password('repeat_password' , array('class' => 'form-control')) }}
+		</div>
+	{{ Form::submit('Sign Up', array('class' => 'btn btn-primary')) }}
+	{{ Form::close() }}	
+@stop
