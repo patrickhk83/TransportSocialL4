1b2f532167a063df96c65554ebb0939196204041
Justin Favaloro <justin@justin.com>
2013-11-02 23:14:07 +1100

    Migration to create flights table

---
+++ app/database/migrations/2013_11_01_154443_create_flights_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateFlightsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('flights', function(Blueprint $table) {
+			$table->increments('id');
+			$table->integer('number');
+			$table->string('carrierFsCode', 6);
+			$table->string('arrivalAirportCode', 6);
+			$table->string('departureAirportCode', 6);
+			$table->datetime('arrivalTime');
+			$table->datetime('departureTime');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('flights');
+	}
+
+}
