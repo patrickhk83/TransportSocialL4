b189b22b0050e3a936789688ac2dcae2fe6c6f24
ddksaku <ddksaku@gmail.com>
2013-11-02 14:40:56 +0800

    Create a user and send email with activation code and create a register form

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -16,14 +16,49 @@ class UsersController extends BaseController {
 		return View::make('users.login');
 	}
 
-	/**
+	public function register() {
+		if(Auth::user()) return Redirect::route('site.home');
+		return View::make('users.register');
+	}
+
+/**
 	 * Show the form for creating a new resource.
 	 *
 	 * @return Response
 	 */
 	public function create()
 	{
-    return View::make('users.create');
+/*		
+		$group = Sentry::createGroup(array(
+			'name' => 'Admins' ,
+			'permissions' => array('admin' => 1, 'users' => 1,),
+		));
+
+		$group1 = Sentry::createGroup(array(
+			'name' => 'Users' ,
+			'permissions' => array('admin' => 0, 'users' => 1,),
+		));
+*/		
+		
+        $validation = new Services\Validators\Register;
+
+		if($validation->passes()) {
+			try
+			{
+				//Register a user.
+				$auth = new Services\Authentication\Auth;
+				$user = $auth->register(Input::all(), 'Users');
+
+			}
+			catch(Cartalyst\Sentry\Users\UserExistsException $e)
+			{
+				Session::flash('error', 'User with this login already exists.');
+				return Redirect::back()->withInput()->withErrors($validation->errors);
+			}
+			return Redirect::route('user.profile', array('id' => $user->id));
+		}
+
+		return Redirect::back()->withInput()->withErrors($validation->errors);
 	}
 
 	public function logout() {
--- app/routes/userRoutes.php
+++ app/routes/userRoutes.php
@@ -3,10 +3,10 @@
 Route::get('login',  array('as' => 'users.login', 'uses' => 'UsersController@login'));
 Route::get('logout',  array('as' => 'users.logout', 'uses' => 'UsersController@logout'));
 
-Route::get('user/{id}/flights', array('as' => 'user.flights', 'uses' => 'UserController@saved_flights'))
+Route::get('user/{id}/flights', array('as' => 'user.flights', 'uses' => 'UsersController@saved_flights'))
 		 ->where(array('id' => '[0-9]+'));
 
-Route::get('user/{id}/profile', array('as' => 'user.profile', 'uses' => 'UserController@profile'))
+Route::get('user/{id}/profile', array('as' => 'user.profile', 'uses' => 'UsersController@profile'))
 		 ->where(array('id' => '[0-9]+'));
 
 Route::post('register', array('as' => 'users.register', 'uses' => 'UsersController@create'));
