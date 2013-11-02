95b02fa7c03d3dc3ccc5150db5b05b48489614bc
justinf111 <justin_f_111@hotmail.com>
2013-11-02 19:09:33 +1100

    Deleted post related files

--- app/views/posts/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("layouts.default")
-@section("content")
-	@foreach ($posts as $post)
-		<div>
-			<span>{{ $post->title }}</span>
-			<span>{{ $post->body }}</span>
-		</div>
-	@endforeach
-@stop
