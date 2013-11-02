5069c28c33678547f05ca0aae8fc6f659bd1e5b5
ddksaku <ddksaku@gmail.com>
2013-11-02 13:46:01 +0800

    Created migration to add extra fields for user.

---
+++ app/database/migrations/2013_11_02_034842_add_user_custom_fields.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddUserCustomFields extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table) {
+			$table->string('company')->nullable();
+			$table->string('phone')->nullable();
+			$table->string('country')->nullable();
+			$table->string('birthday')->nullable();
+			$table->string('about_me')->nullable();
+			$table->string('hobbies')->nullable();
+			$table->string('musics')->nullable();
+			$table->string('movies')->nullable();
+			$table->string('books')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table) {
+			$table->dropColumn('company');
+			$table->dropColumn('phone');
+			$table->dropColumn('country');
+			$table->dropColumn('birthday');
+			$table->dropColumn('about_me');
+			$table->dropColumn('hobbies');
+			$table->dropColumn('musics');
+			$table->dropColumn('movies');
+			$table->dropColumn('books');
+		});
+	}
+
+}
