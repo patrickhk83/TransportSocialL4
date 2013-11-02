f60888b3706364c441e209e2b976400df4568a55
ddksaku <ddksaku@gmail.com>
2013-11-02 12:27:20 +0800

    Added language file for user auth

---
+++ app/lang/en/user_auth.php
@@ -0,0 +1,9 @@
+<?php 
+
+return array(
+	'create_user_fname_label'	=> 'First Name:',
+	'create_user_lname_label'	=> 'Last Name:',
+	'create_user_email_label'	=> 'Email:',	
+	'create_user_password_label'	=> 'Password:',
+	'create_user_password_confirm_label'	=> 'Confirm:',
+);
