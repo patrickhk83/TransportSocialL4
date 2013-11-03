81630cd06ce52187ae7e10c49a33934b3419a91f
justinf111 <justin_f_111@hotmail.com>
2013-11-03 21:26:23 +1100

    Refactored the way the date is pretend for the api request url

--- app/services/flightstat/FlightStatus.php
+++ app/services/flightstat/FlightStatus.php
@@ -14,41 +14,25 @@ class FlightStatus extends FlightstatApi {
 	}
 
 	public function by_airport($request) {
-		$date = $this->split_date($request['date']);
     $function = 'airport/status/'.$request['arrivalAirportCode'].'/'.
-                $request['direction'].'/'.$this->format_date($date).'/'.$request['hour'];
+                $request['direction'].'/'.$this->format_date($request['date']).'/'.$request['hour'];
     return $this->api_call($function, $this->queries);
 	}
 
 	public function by_route($request) {
-		$date = $this->split_date($request['date']);
     $function = 'route/status/'.$request['departureAirportCode'].'/'.
-                $request['arrivalAirportCode'].'/dep/'.$this->format_date($date);
+                $request['arrivalAirportCode'].'/dep/'.$this->format_date($request['date']);
 
     return $this->api_call($function, $this->queries);
 	}
 
 	public function by_flight_num($request) {
-		$date = $this->split_date($request['date']);
     $function = 'flight/status/'.$request['carrierCode'].'/'.
-                 $request['flightNumber'].'/dep/'.$this->format_date($date);
+                 $request['flightNumber'].'/dep/'.$this->format_date($request['date']);
     return $this->api_call($function, $this->queries);
 	}
 
 	public function format_date($date) {
-    return
-      $date['year'].'/'.
-      $date['month'].'/'.
-      $date['day'];
-  }
-
-  public function split_date($date) {
-    $date = explode('-', $date);
-    $date = array(
-      'day' => $date[0],
-      'month' => $date[1],
-      'year' => $date[2]
-    );
-    return $date;
+    return date("Y/n/j", strtotime($date));
   }
 }
