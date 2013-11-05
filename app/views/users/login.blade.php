d7f0dba99774639fb7903adecd63ab4a23feb0d8
ddksaku <ddksaku@gmail.com>
2013-11-05 16:08:45 +0800

    Modified ID of element named "passwords"

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.default')
 @section('content')
-	{{ Form::open(array('action', 'UsersController@login'))}}
+	@include('_partials.errors')
+	{{ Form::open(array('route' => 'users.auth', 'id' => 'login_form'))}}
 	<div class="form-group">
 		<label for="username">Username</label>
 		{{ Form::text('username', null, array('class' => 'form-control')) }}
 	</div>
 	<div class="form-group">
-		<label for="password">Password</label>
-		{{ Form::text('password', null, array('class' => 'form-control')) }}
+		<label for="passwords">Password</label>
+		{{ Form::text('passwords', null, array('class' => 'form-control')) }}
 	</div>
 	{{ Form::submit('Login', array('class' => 'btn btn-primary')) }}
 	{{ Form::close() }}
