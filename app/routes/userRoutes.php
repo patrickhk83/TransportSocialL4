313c77492497015de513fa4ee185804199e2e71b
justinf111 <justin_f_111@hotmail.com>
2013-11-09 18:24:30 +1100

    Move route from users to flights

--- app/routes/userRoutes.php
+++ app/routes/userRoutes.php
@@ -1,21 +1,19 @@
 <?php
 
 Route::get('login',  array('as' => 'users.login', 'uses' => 'UsersController@login'));
-
 Route::post('login',  array('as' => 'users.auth', 'uses' => 'UsersController@auth'));
 
 Route::get('logout',  array('as' => 'users.logout', 'uses' => 'UsersController@logout'));
-
-Route::get('user/{id}/flights', array('as' => 'user.flights', 'uses' => 'UsersController@saved_flights'))
-		 ->where(array('id' => '[0-9]+'));
 
 Route::get('user/{id}/profile', array('as' => 'user.profile', 'uses' => 'UsersController@profile'))
 		 ->where(array('id' => '[0-9]+'));
 
 Route::post('register', array('as' => 'users.register', 'uses' => 'UsersController@create'));
-
 Route::get('register', array('as' => 'users.registerForm', 'uses' => 'UsersController@register'));
 
-Route::get('user/edit_profile' , array('as' => 'users.edit_profile' , 'uses' => 'UsersController@edit_profile'));
+Route::group(array('before' => 'auth'), function()
+{
+	Route::get('user/profile/edit' , array('as' => 'user.edit_profile' , 'uses' => 'UsersController@edit_profile'));
+	Route::post('user/profile/edit' , array('as' => 'user.update' , 'uses' => 'UsersController@update'));
+});
 
-Route::post('user/edit_profile' , array('as' => 'users.update' , 'uses' => 'UsersController@update'));
