c231dc098529e22f9c13f1401e0f9dfcc106fb9e
justinf111 <justin_f_111@hotmail.com>
2013-11-11 23:10:42 +1100

    Save photo path as the url instead file directory path

--- app/services/Image.php
+++ app/services/Image.php
@@ -14,11 +14,12 @@ class Image {
 	public function upload($file , $user_id , $directory)
 	{
 		$file_name = $file->getClientOriginalName();
-		$path = "\\images\\".$directory;
+		$path = public_path()."\\images\\".$directory."\\";
+		$url = "/images/".$directory."/".$file_name;
 		$real_extension = $file->getClientOriginalExtension();
-		$dest_file = $path."\\".$file_name;
+		$dest_file = $path.$file_name;
 		if($file->move($path , $dest_file)) {
-			$this->path = $dest_file;
+			$this->path = $url;
 		}
 		else {
 			$errors[] = 'Unable to upload image';
