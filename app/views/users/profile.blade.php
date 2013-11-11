20c39bc7fb0804963db33a5449d3514415a50176
justinf111 <justin_f_111@hotmail.com>
2013-11-11 23:11:07 +1100

    get path property instead of whole photo object

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -7,7 +7,7 @@
 	<div>
 		<h1>{{$user->first_name." ".$user->last_name;}}</h1>
 		{{ link_to_route('user.edit_profile', 'Edit Profile', null, array('class' => 'btn btn-primary')) }}
-	</div>	
+	</div>
 	<div class="photo">
 		{{ HTML::image($profile_pic , null , array('class' => 'thumb')) }}
 		{{ link_to_route('user.edit_profile', 'Edit Profile Pic', null, array('class' => 'btn btn-primary' , 'data-toggle' => 'modal' , 'data-target' => '#profile_pic_form' , 'data-remote' => 'false')) }}
@@ -19,22 +19,22 @@
 		</p>
 	</div>
 	<?php endif; ?>
-	
+
 	<?php if(!empty($user->about_me)): ?>
 		<h1 class="heading">{{ trans('user_auth.my_profile_about_me'); }}</h1>
 		<p class="content">{{ $user->about_me; }}</p>
 	<?php endif; ?>
-	
+
 	<?php if(!empty($user->hobbies)): ?>
 		<h1 class="heading">{{ trans('user_auth.my_profile_hobbies'); }}</h1>
 		<p class="content">{{ $user->hobbies; }}</p>
 	<?php endif; ?>
-	
+
 	<?php if(!empty($user->musics)): ?>
 		<h1 class="heading">{{ trans('user_auth.my_profile_musics'); }}</h1>
 		<p class="content">{{ $user->musics; }}</p>
 	<?php endif; ?>
-	
+
 	<?php if(!empty($user->movies)): ?>
 		<h1 class="heading">{{ trans('user_auth.my_profile_movies'); }}</h1>
 		<p class="content">{{ $user->movies; }}</p>
@@ -48,10 +48,10 @@
 	<h1 class="heading">{{ trans('user_auth.my_profile_my_photos'); }}</h1>
 		<div>
 			<?php foreach ($photos as $photo): ?>
-				{{ HTML::image($photo , null , array('class' => 'thumb')) }}
+				{{ HTML::image($photo->path , null , array('class' => 'thumb')) }}
 			<?php endforeach; ?>
 		</div>
 	<?php endif ?>
-	
+
 @include('users.profile_pic');
 @stop
