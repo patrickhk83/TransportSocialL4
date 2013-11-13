2241d9c75f7c250b2da6dc6af9099ef936d92b1e
justinf111 <justin_f_111@hotmail.com>
2013-11-13 22:29:04 +1100

    Add route for adding photos

--- app/routes/userRoutes.php
+++ app/routes/userRoutes.php
@@ -20,5 +20,6 @@ Route::group(array('before' => 'auth'), function()
 Route::get('user/{id}/activate/{activation_code}', array('as' => 'user.activate', 'uses' => 'UsersController@activate'))
 		->where(array('id' => '[0-9]+'));
 
-Route::post('user/profile/edit_profile_pic' , array('as' => 'user.edit_profile_pic' , 'uses' => 'UsersController@edit_profile_pic'));		
+Route::post('user/profile/edit_profile_pic' , array('as' => 'user.edit_profile_pic' , 'uses' => 'UsersController@edit_profile_pic'));
 
+Route::post('user/profile/add_photo' , array('as' => 'user.add_photo' , 'uses' => 'UsersController@add_photo'));
