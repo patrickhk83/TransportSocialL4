09615102909987a1d5f82c62af0ba604ad69fb66
justinf111 <justin_f_111@hotmail.com>
2013-11-13 22:10:46 +1100

    updated profile template with blade tags, and included multifile javascript file

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.default')
 
 @section('content')
+	{{ HTML::style('css/jquery.fileupload.css'); }}
+	{{ HTML::style('css/jquery.fileupload-ui.css'); }}
 
 	{{ HTML::script('js/modal.js'); }}
 	{{ HTML::script('js/manageuser.js'); }}
+	{{ HTML::script('js/multifile.js'); }}
 	<div>
 		<h1>{{$user->first_name." ".$user->last_name;}}</h1>
 		{{ link_to_route('user.edit_profile', 'Edit Profile', null, array('class' => 'btn btn-primary')) }}
@@ -12,46 +15,47 @@
 		{{ HTML::image($profile_pic , null , array('class' => 'thumb')) }}
 		{{ link_to_route('user.edit_profile', 'Edit Profile Pic', null, array('class' => 'btn btn-primary' , 'data-toggle' => 'modal' , 'data-target' => '#profile_pic_form' , 'data-remote' => 'false')) }}
 	</div>
-	<?php if(!empty($user->company)): ?>
-	<div class="occupation">
-		<p>
-			{{ $user->company. ' | ' .$country->name; }}
-		</p>
-	</div>
-	<?php endif; ?>
+	@if(!empty($user->company))
+		<div class="occupation">
+			<p>{{ $user->company. ' | ' .$country->name; }}</p>
+		</div>
+	@endif
 
-	<?php if(!empty($user->about_me)): ?>
+	@if(!empty($user->about_me))
 		<h1 class="heading">{{ trans('user_auth.my_profile_about_me'); }}</h1>
 		<p class="content">{{ $user->about_me; }}</p>
-	<?php endif; ?>
+	@endif
 
-	<?php if(!empty($user->hobbies)): ?>
+	@if(!empty($user->hobbies))
 		<h1 class="heading">{{ trans('user_auth.my_profile_hobbies'); }}</h1>
 		<p class="content">{{ $user->hobbies; }}</p>
-	<?php endif; ?>
+	@endif
 
-	<?php if(!empty($user->musics)): ?>
+	@if(!empty($user->musics))
 		<h1 class="heading">{{ trans('user_auth.my_profile_musics'); }}</h1>
 		<p class="content">{{ $user->musics; }}</p>
-	<?php endif; ?>
+	@endif
 
-	<?php if(!empty($user->movies)): ?>
+	@if(!empty($user->movies))
 		<h1 class="heading">{{ trans('user_auth.my_profile_movies'); }}</h1>
 		<p class="content">{{ $user->movies; }}</p>
-	<?php endif; ?>
-	<?php if(!empty($user->books)): ?>
+	@endif
+	@if(!empty($user->books))
 		<h1 class="heading">{{ trans('user_auth.my_profile_books'); }}</h1>
 		<p class="content">{{ $user->books; }}</p>
-	<?php endif; ?>
+	@endif
 
-	<?php if (isset($photos) && count($photos) > 0): ?>
+	@if (isset($photos) && count($photos) > 0)
 	<h1 class="heading">{{ trans('user_auth.my_profile_my_photos'); }}</h1>
 		<div>
-			<?php foreach ($photos as $photo): ?>
-				{{ HTML::image($photo->path , null , array('class' => 'thumb')) }}
-			<?php endforeach; ?>
+			@foreach ($photos as $photo)
+				@if($photo->id != $user->profile_pic)
+					{{ HTML::image($photo , null , array('class' => 'thumb')) }}
+				@endif
+			@endforeach
 		</div>
-	<?php endif ?>
+	@endif
+	{{ link_to_route('user.add_photo', 'Add My Photo', null, array('class' => 'btn btn-primary' , 'data-toggle' => 'modal' , 'data-target' => '#upload_photo_dialog' , 'data-remote' => 'false')) }}
 
-@include('users.profile_pic');
+@include('users.profile_pic')
 @stop
