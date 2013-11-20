c8780b9096cb3783de7937e3e725feac16768266
ddksaku@gmail.com <ddksaku@gmail.com>
2013-11-20 18:13:13 +0800

    added user photo diadlog form

--- app/views/users/profile_pic.blade.php
+++ app/views/users/profile_pic.blade.php
@@ -4,7 +4,7 @@
 	        <div class="modal-content">
 	            <div class="modal-header">
 	                <a class="close" data-dismiss="modal">x</a>
-	                <h4>Upload Your Picture</h4>
+	                <h4>Upload Your Profile Picture</h4>
 	            </div>
 	            <div class="modal-body">
 	            	<fieldset  id="user_photo_info">
@@ -30,4 +30,40 @@
 	        </div>
 	    </div>
 	</div>         
+{{ Form::close(); }}
+
+{{ Form::open(array('route' => 'user.add_photo' , 'files' => true , 'id' => 'upload_photo_form')); }}
+	<div id="upload_photo_dialog" class="modal fade"  role="dialog" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<a class="close" data-dismiss="modal">x</a>
+					<h4>Upload Your Photo</h4>
+					{{ Form::button('Close', array('class' => 'btn' , 'data-dismiss' => 'modal' , 'aria-hidden' => 'true')) }}
+	            	{{ Form::submit('Submit' , array('class' => 'btn btn-primary')); }}
+
+				</div>	
+				<div class="modal-body">
+					<fieldset id="user_photo_field">
+						<div class="row">
+	                        <div class="col-md-6 file-wrapper">
+	                            <span class="btn btn-success fileinput-button"  >
+	                                <span class="fileinput-button">
+	                                    <i class="glyphicon glyphicon-plus"></i>
+	                                    <span>Browse...</span>
+	                                    <input type="file" name="file[]" id="multifile_input">
+	                                </span>
+	                            </span>
+	                        </div>							
+						</div>
+						<div class="row">
+							<ul id="multifile_list" class="files"></ul>
+						</div>	
+					</fieldset>	
+				</div>
+				<div class="modal-footer">
+				</div>	
+			</div>	
+		</div>	
+	</div>	
 {{ Form::close(); }}
