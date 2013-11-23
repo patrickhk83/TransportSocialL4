e93bab293660703548f735a31ff42120ee26d4b2
justinf111 <justin_f_111@hotmail.com>
2013-11-23 15:56:42 +1100

    Removed activation link from lang file

--- app/lang/en/user_auth.php
+++ app/lang/en/user_auth.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 return array(
 	'create_user_fname_label'	=> 'First Name:',
 	'create_user_lname_label'	=> 'Last Name:',
-	'create_user_email_label'	=> 'Email:',	
+	'create_user_email_label'	=> 'Email:',
 	'create_user_password_label'	=> 'Password:',
 	'create_user_password_confirm_label'	=> 'Confirm:',
 	'create_user_activate_label'	=> 'Activate account for %s',
-	'create_user_activate_subheading'	=> 'Please click this link to %s.',
+	'create_user_activate_subheading'	=> 'Please click this link to',
 	'my_profile_about_me'		=>		'About Me',
 	'my_profile_hobbies'		=>		'Hobbies',
 	'my_profile_musics'			=> 		'Favorite Musics',
@@ -23,7 +23,7 @@ return array(
 	'create_user_hobbies'					=>	'Hobbies: ',
 	'create_user_musics'						=>	'Musics: ',
 	'create_user_movies'					=>	'Movies: ',
-	'create_user_books'						=>	'Books: ',	
-	'create_user_my_profile_pic_info'				=>	'My Profile Picture',	
+	'create_user_books'						=>	'Books: ',
+	'create_user_my_profile_pic_info'				=>	'My Profile Picture',
 	'create_user_photos_info'				=>	'My Photos'
 );
