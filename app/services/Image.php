566b0f9ef52f63af34370e00fd79c45f815b7c79
justinf111 <justin_f_111@hotmail.com>
2013-11-23 15:55:59 +1100

    Renamed folder from images to uploads, and removed create photo function

--- app/services/Image.php
+++ app/services/Image.php
@@ -14,8 +14,8 @@ class Image {
 	public function upload($file , $user_id , $directory)
 	{
 		$file_name = $file->getClientOriginalName();
-		$path = public_path()."\\images\\".$directory."\\";
-		$url = "/images/".$directory."/".$file_name;
+		$path = public_path()."\\uploads\\".$directory."\\";
+		$url = "/uploads/".$directory."/".$file_name;
 		$real_extension = $file->getClientOriginalExtension();
 		$dest_file = $path.$file_name;
 		if($file->move($path , $dest_file)) {
@@ -24,14 +24,5 @@ class Image {
 		else {
 			$errors[] = 'Unable to upload image';
 		}
-	}
-
-	public function create($path , $user_id)
-	{
-		$photo = new \Photo;
-		$photo->user_id = $user_id;
-		$photo->path = $path;
-		$photo->save();
-		return $photo;
 	}
 }
