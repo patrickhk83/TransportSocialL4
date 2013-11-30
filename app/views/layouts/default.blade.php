f6f3d02ceae9ea655923ecd0fc53e284743ddf3d
justinf111 <justin_f_111@hotmail.com>
2013-12-01 01:47:50 +1100

    echo out styles and scripts for assets container

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
     {{ HTML::style("//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css") }}
     {{ HTML::style("css/style.css") }}
     {{ HTML::style('css/jquery-ui.css') }}
+    {{ Asset::container('assets')->styles() }}
     @yield('stylesheets')
     <meta charset="UTF-8" />
     <title>
@@ -26,6 +27,7 @@
     {{ HTML::script("http://code.jquery.com/jquery-latest.min.js") }}
     {{ Html::script("http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.js") }}
     {{ HTML::script("//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js") }}
+    {{ Asset::container('assets')->scripts() }}
     @yield('scripts')
   </body>
 </html>
