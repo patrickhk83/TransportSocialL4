d6b60480414aad16a026703dc350428cece9172e
justinf111 <justin_f_111@hotmail.com>
2013-11-30 13:02:54 +1100

    Added link to activate route

--- app/views/emails/auth/activateuser.blade.php
+++ app/views/emails/auth/activateuser.blade.php
@@ -4,10 +4,10 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<h2>{{ sprintf(trans('user_auth.create_user_activate_label') , $name);}}</h2>
+		<h2>{{ sprintf(trans('user_auth.create_user_activate_label') , $name) }}</h2>
 
 		<div>
-			{{ sprintf(trans('user_auth.create_user_activate_subheading') , URL::to('user.activate' , array('id' => $id , 'activation' => $activation)))}}
+			{{ trans('user_auth.create_user_activate_subheading').' '.link_to_route('user.activate', 'Activate Account', array('id' => $id , 'activation_code' => $activation))}}
 		</div>
 	</body>
 </html>
