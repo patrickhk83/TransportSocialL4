35a1ec73fc36def0cefedb24d4c22d3fbd384b02
justinf111 <justin_f_111@hotmail.com>
2013-11-30 14:24:24 +1100

    Removed header and changed text for register button

--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -2,22 +2,18 @@
 @section('content')
 	{{ HTML::script("js/manageuser.js") }}
 
-	<div class="page-header">
-		<h1>Signup Now For a Account</h1>
-	</div>	
-
 	@include('_partials.errors')
 	{{ Form::open(array('route' => 'users.register', 'id' => 'customer_form'))}}
 		<input type="hidden" name="csrf_token" value="" />
 		<div class="form-group">
 			{{ Form::label('first_name' , trans('user_auth.create_user_fname_label'))}}
 			{{ Form::text('first_name', null, array('class' => 'form-control')) }}
-		</div> 	
+		</div>
 
 		<div class="form-group">
 			{{ Form::label('last_name' , trans('user_auth.create_user_lname_label'))}}
 			{{ Form::text('last_name', null, array('class' => 'form-control')) }}
-		</div>	
+		</div>
 
 		<div class="form-group">
 			{{ Form::label('email' , trans('user_auth.create_user_email_label'))}}
@@ -33,6 +29,6 @@
 			{{ Form::label('repeat_password' , trans('user_auth.create_user_password_confirm_label'))}}
 			{{ Form::password('repeat_password' , array('class' => 'form-control')) }}
 		</div>
-	{{ Form::submit('Sign Up', array('class' => 'btn btn-primary')) }}
-	{{ Form::close() }}	
+	{{ Form::submit('Register', array('class' => 'btn btn-primary')) }}
+	{{ Form::close() }}
 @stop
