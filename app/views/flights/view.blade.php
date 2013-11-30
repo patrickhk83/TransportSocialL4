6df2be80cf5369d22c9a58054b1139a640e2a9a1
justinf111 <justin_f_111@hotmail.com>
2013-11-30 14:22:13 +1100

    Added view for viewing flights

---
+++ app/views/flights/view.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.default')
+@section('content')
+  <div class="flightNumber">
+    {{ $flight->carrier->iata }}
+    {{ $flight->flightNumber }}
+  </div>
+  <div class="carrier">
+    {{ $flight->carrier->name }}
+  </div>
+  <div class="route">
+    {{ $flight->arrivalAirport->name.' to '.$flight->departureAirport->name }}
+  </div>
+
+  <div class="times">
+    <p>Departure Time: {{ date('d/m/Y h:m', strtotime($flight->departureDate)) }}</p>
+    <p>Arrival Time: {{ date('d/m/Y h:m', strtotime($flight->arrivalDate)) }}</p>
+  </div>
+
+  @if(count($flight->passengers) > 0)
+    <p>
+      @foreach($flight->passengers as $passenger)
+        <img src="/assets/images/default-profile-pic.png" width="20" height="20">
+      @endforeach
+    </p>
+  @endif
+
+  @if(Sentry::check())
+    @if(!$flight->saved)
+      {{ link_to_route('flight.privacy', 'Save', array($flight->id), array('class' => 'btn btn-primary')) }}
+    @else
+      {{ link_to_route('flight.delete', 'Delete', array($flight->id), array('class' => 'btn btn-primary')) }}
+    @endif
+  @endif
+@stop
