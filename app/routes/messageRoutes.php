c4b77fc505dfe139edc8c4fbb9a13f4bd98ef613
justinf111 <justin_f_111@hotmail.com>
2013-11-30 13:15:59 +1100

    Added route for sending messages and group routes to have auth filter

--- app/routes/messageRoutes.php
+++ app/routes/messageRoutes.php
@@ -1,10 +1,15 @@
 <?php
 
-Route::get('messages/inbox' , array('as' => 'messages.inbox', 'uses' => 'MessagesController@inbox'));
+Route::group(array('before' => 'auth'), function()
+{
+	Route::get('messages/inbox' , array('as' => 'messages.inbox', 'uses' => 'MessagesController@inbox'));
 
-Route::get('messages/contacts' , array('as' => 'messages.contacts' , 'uses' => 'MessagesController@contacts'));
+	Route::get('messages/contacts' , array('as' => 'messages.contacts' , 'uses' => 'MessagesController@contacts'));
 
-Route::post('messages/add_contact' , array('as' => 'messages.add_contact' , 'uses' => 'MessagesController@add_contact'));
+	Route::post('messages/add_contact' , array('as' => 'messages.add_contact' , 'uses' => 'MessagesController@add_contact'));
 
-Route::post('messages/suggest_user' , array('as' => 'messages.suggest_user' , 'uses' => 'MessagesController@suggest_user'));
-//Route::post('login',  array('as' => 'users.auth', 'uses' => 'UsersController@auth'));
+	Route::post('messages/suggest_user' , array('as' => 'messages.suggest_user' , 'uses' => 'MessagesController@suggest_user'));
+
+	Route::any('messages/send/{conversationId}', array('as' => 'message.send', 'uses' => 'MessagesController@send'))
+				->where(array('conversationId' => '[0-9]+'));;
+});
