a925b852372c7da0174abd3f55c7af3a8976a908
justinf111 <justin_f_111@hotmail.com>
2013-11-30 14:21:58 +1100

    Loaded dependencies and added send function

--- app/controllers/MessagesController.php
+++ app/controllers/MessagesController.php
@@ -1,19 +1,28 @@
 <?php
 
 use Repositories\User\UserRepositoryInterface as User;
+use Repositories\Message\MessageRepositoryInterface as Message;
+use Repositories\Conversation\ConversationRepositoryInterface as Conversation;
 
 class MessagesController extends BaseController {
 
 	protected $users;
+	protected $messages;
+	protected $conversations;
 
-	public function __construct(User $users) {
+	public function __construct(User $users, Message $messages, Conversation $conversations) {
 		$this->users = $users;
+		$this->messages = $messages;
+		$this->conversations = $conversations;
 	}
 
 	public function inbox()
 	{
 		$auth = new Services\Auth;
-		$data['user'] = $auth->GetUserInfo();
+		$user = $auth->GetUserInfo();
+
+		$data['conversations'] = $this->users->get_conversations($user->id);
+		$data['user'] = $user;
 
 		return View::make('messages.inbox')->with($data);
 	}
@@ -31,7 +40,6 @@ class MessagesController extends BaseController {
 	{
 		$msg = new Services\PrivateMessage;
 		return $msg->suggest_user(Input::get('term'));
-
 	}
 
 	public function add_contact()
@@ -53,6 +61,15 @@ class MessagesController extends BaseController {
 		}
 
 		return Redirect::back()->withInput()->withErrors($validation->errors);
+	}
+
+	public function send($conversationId) {
+		$auth = new Services\Auth;
+		$user = $auth->GetUserInfo();
+		$user = $this->users->find($user->id);
+		$conversation = $this->conversations->find($conversationId);
+		$message = $this->messages->create(Input::all(), $conversation, $user);
+		return Redirect::route('conversation.view', array('id' => $conversationId));
 	}
 
 }
