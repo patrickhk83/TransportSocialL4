c3d381a4c4f4a874ab872e4aa9a223a0b4eff611
justinf111 <justin_f_111@hotmail.com>
2013-12-05 22:09:43 +1100

    Adjusted routes to use guest and auth filters where needed

--- app/routes/userRoutes.php
+++ app/routes/userRoutes.php
@@ -1,25 +1,27 @@
 <?php
 
-Route::get('login',  array('as' => 'users.login', 'uses' => 'UsersController@login'));
-Route::post('login',  array('as' => 'users.auth', 'uses' => 'UsersController@auth'));
+Route::group(array('before' => 'guest'), function()
+{
+	Route::get('login',  array('as' => 'users.login', 'uses' => 'UsersController@login'));
+	Route::post('login',  array('as' => 'users.auth', 'uses' => 'UsersController@auth'));
 
-Route::get('logout',  array('as' => 'users.logout', 'uses' => 'UsersController@logout'));
-
-Route::get('user/{id}/profile', array('as' => 'user.profile', 'uses' => 'UsersController@profile'))
-		 ->where(array('id' => '[0-9]+'));
-
-Route::post('register', array('as' => 'users.register', 'uses' => 'UsersController@create'));
-Route::get('register', array('as' => 'users.registerForm', 'uses' => 'UsersController@register'));
+	Route::post('register', array('as' => 'users.register', 'uses' => 'UsersController@create'));
+	Route::get('register', array('as' => 'users.registerForm', 'uses' => 'UsersController@register'));
+});
 
 Route::group(array('before' => 'auth'), function()
 {
+	Route::get('logout',  array('as' => 'users.logout', 'uses' => 'UsersController@logout'));
+
+	Route::get('user/{id}/profile', array('as' => 'user.profile', 'uses' => 'UsersController@profile'))
+		 ->where(array('id' => '[0-9]+'));
+
+	Route::post('user/profile/edit_profile_pic' , array('as' => 'user.edit_profile_pic' , 'uses' => 'UsersController@edit_profile_pic'));
+	Route::post('user/profile/add_photo' , array('as' => 'user.add_photo' , 'uses' => 'UsersController@add_photo'));
+
 	Route::get('user/profile/edit' , array('as' => 'user.edit_profile' , 'uses' => 'UsersController@edit_profile'));
 	Route::post('user/profile/edit' , array('as' => 'user.update' , 'uses' => 'UsersController@update'));
 });
 
 Route::get('user/{id}/activate/{activation_code}', array('as' => 'user.activate', 'uses' => 'UsersController@activate'))
 		->where(array('id' => '[0-9]+'));
-
-Route::post('user/profile/edit_profile_pic' , array('as' => 'user.edit_profile_pic' , 'uses' => 'UsersController@edit_profile_pic'));
-
-Route::post('user/profile/add_photo' , array('as' => 'user.add_photo' , 'uses' => 'UsersController@add_photo'));
