7e359d0a36a76cb48ac5be45278c6becc83bf1af
justinf111 <justin_f_111@hotmail.com>
2013-12-05 22:53:08 +1100

    Added my ip so I can access web command line

--- app/config/packages/jones/web-artisan/config.php
+++ app/config/packages/jones/web-artisan/config.php
@@ -4,6 +4,6 @@ return array(
 	'enable' => true,
 	'base_url' => 'web-artisan',
 	'password' => 'transocpassword',
-	'ips' => array('127.0.0.1', '::1'),
+	'ips' => array('60.242.31.126', '::1'),
 	'auth_filter' => '',
 );
