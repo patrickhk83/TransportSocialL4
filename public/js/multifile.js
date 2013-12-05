616a54b15c34f392a349911ad09988954376a61e
justinf111 <justin_f_111@hotmail.com>
2013-12-05 22:07:28 +1100

    Refactored the multifile js file.

--- public/js/multifile.js
+++ public/js/multifile.js
@@ -2,53 +2,36 @@ $("document").ready(function(){
 	$('#multifile_input').change(function(evt)
 	{
 		var files = evt.target.files;
-		var file = files[0];
-		var id = this;
-		if(!file.type.match('image/gif') && !file.type.match('image/jpg') && !file.type.match('image/png') && !file.type.match('image/jpeg'))
-		{
-			return;
-		}
+        for(var i = 0; i < files.length; i++) {
+            var file = files[i];
+            var id = this;
+            if(!file.type.match('image/gif') && !file.type.match('image/jpg') && !file.type.match('image/png') && !file.type.match('image/jpeg'))
+            {
+                return;
+            }
+            var reader = new FileReader();
 
-		new_input = $(id).clone(true);
-		new_input.off();
-		$(new_input).hide();
-		$(id.parentNode).append(new_input);
+            reader.onload = (function(theFile)
+            {
+                return function(e) {
+                    var new_li = document.createElement('li');
+                    var new_img = document.createElement('img');
+                    new_img.src = e.target.result;
+                    new_img.classList.add('thumb');
+                    var new_name = document.createElement('span');
+                    new_name.innerHTML = escape(theFile.name);
 
-		var reader = new FileReader();
+                    $(new_li).prepend(delete_link);
+                    $(new_li).prepend(new_name);
+                    $(new_li).prepend(new_img);
 
-		reader.onload = (function(theFile) 
-		{
-			return function(e) {
-				var new_li = document.createElement('li');
-				//new_li.classList.add('template-upload fade in');
-				var new_img = document.createElement('img');
-				new_img.src = e.target.result;
-				new_img.classList.add('thumb');
-				var new_name = document.createElement('span');
-				new_name.innerHTML = escape(theFile.name);
 
-				var delete_link = document.createElement('a');
-				delete_link.innerHTML = "<span class='btn btn-danger delete'><i class='glyphicon glyphicon-trash'></i><span>Delete</span></span>";
+                    $('#multifile_list').append(new_li);
+                }
+            })(file);
 
-				delete_link.href = "#";
-				delete_link.input_reference = new_input;
-			
-				$(delete_link).click(function() {
-					$(this.input_reference).remove();
-					$(this.parentNode).remove();
-				});
+            reader.readAsDataURL(file);
+        }
 
-				$(new_li).prepend(delete_link);
-				$(new_li).prepend(new_name);
-				$(new_li).prepend(new_img);
-				
-				
-				$('#multifile_list').append(new_li);
-				id.value = '';
-
-			}
-		})(file);
-
-		reader.readAsDataURL(file);
-	});	
+	});
 });
