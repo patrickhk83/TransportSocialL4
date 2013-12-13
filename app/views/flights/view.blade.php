bcacf279ab71614c11c6f08bdd3b3add4ddcc816
justinf111 <justin_f_111@hotmail.com>
2013-12-13 23:10:01 +1100

    Use const date format and get user profile pic if set

--- app/views/flights/view.blade.php
+++ app/views/flights/view.blade.php
@@ -12,14 +12,18 @@
   </div>
 
   <div class="times">
-    <p>Departure Time: {{ date('d/m/Y h:m', strtotime($flight->departureDate)) }}</p>
-    <p>Arrival Time: {{ date('d/m/Y h:m', strtotime($flight->arrivalDate)) }}</p>
+    <p>Departure Time: {{ date(DATE_FORMAT, strtotime($flight->departureTime)) }}</p>
+    <p>Arrival Time: {{ date(DATE_FORMAT, strtotime($flight->arrivalTime)) }}</p>
   </div>
 
   @if(count($flight->passengers) > 0)
     <p>
       @foreach($flight->passengers as $passenger)
-        <img src="/images/default-profile-pic.png" width="20" height="20">
+        @if($passenger->profilePicture['path'] != '')
+          <img src="{{ $passenger->profilePicture['path'] }}" width="20" height="20" />
+        @else
+          <img src="{{ DEFAULT_PROFILE_IMG }}" width="20" height="20" />
+        @endif
       @endforeach
     </p>
   @endif
