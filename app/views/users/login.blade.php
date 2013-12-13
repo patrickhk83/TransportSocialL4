55d035949facf9baa2b551588a749d627460e89c
justinf111 <justin_f_111@hotmail.com>
2013-12-13 23:03:21 +1100

    changed username to email and renamed passwords to password

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -3,12 +3,12 @@
 	@include('_partials.errors')
 	{{ Form::open(array('route' => 'users.auth', 'id' => 'login_form'))}}
 	<div class="form-group">
-		<label for="username">Username</label>
-		{{ Form::text('username', null, array('class' => 'form-control')) }}
+		<label for="email">Email</label>
+		{{ Form::text('email', null, array('class' => 'form-control')) }}
 	</div>
 	<div class="form-group">
-		<label for="passwords">Password</label>
-		{{ Form::password('passwords', array('class' => 'form-control')) }}
+		<label for="password">Password</label>
+		{{ Form::password('password', array('class' => 'form-control')) }}
 	</div>
 	{{ Form::submit('Login', array('class' => 'btn btn-primary')) }}
 	{{ link_to_route('users.register', 'Register', null, array('class' => 'btn btn-primary')) }}
