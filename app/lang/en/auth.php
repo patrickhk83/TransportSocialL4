57ef9ddcb42035a97eee8dea77497a6e719e3054
justinf111 <justin_f_111@hotmail.com>
2013-12-13 23:01:39 +1100

    Added lang files for auth errors and flight errors

---
+++ app/lang/en/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'user_not_found' => 'User not found.',
+	'wrong_password' => 'Wrong password, try again.',
+	'password_required' => 'Password field is required.',
+	'user_not_activated' => 'User is not activated.',
+	'user_already_activated' => 'User is not activated.',
+	'user_exists' => 'User with this login already exists.',
+);
