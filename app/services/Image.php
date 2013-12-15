3be318d320e681e2eac852afc5cb95f4f1e57de0
justinf111 <justin_f_111@hotmail.com>
2013-12-15 17:37:43 +1100

    When creating an image it will save a new file for each size from image config

--- app/services/Image.php
+++ app/services/Image.php
@@ -13,16 +13,36 @@ class Image {
 
 	public function upload($file, $directory)
 	{
-		$file_name = $file->getClientOriginalName();
-		$path = public_path()."/uploads/".$directory."/";
-		$url = "/uploads/".$directory."/".$file_name;
-		$real_extension = $file->getClientOriginalExtension();
-		$dest_file = $path.$file_name;
-		if($file->move($path , $dest_file)) {
-			$this->path = $url;
-		}
-		else {
-			$errors[] = 'Unable to upload image';
-		}
+		$filename = $file->getClientOriginalName();
+		$path = \Config::get('image.upload_path').$directory."/";
+		$url = "/uploads/".$directory."/".$filename;
+		$dest_file = $path.$filename;
+		$ext = strtolower($file->getClientOriginalExtension());
+
+		$dimensions = \Config::get('image.dimensions');
+
+		$file->move($path, $filename);
+		$paths = array();
+		$paths['orignal'] = $url;
+				foreach ($dimensions as $key => $dimension)
+    {
+      // Get dimmensions and quality
+      $width   = (int) $dimension[0];
+      $height  = (int) $dimension[1];
+      $crop    = (bool) $dimension[2];
+
+      // Run resizer
+      $image = \Image::make($dest_file);
+
+      if($crop) {
+      	$image = $image->grab($width, $height);
+      }
+      else {
+      	$image = $image->resize($width, $height, true, false);
+      }
+      $paths[$key] = "/uploads/".$directory."/".basename($filename, '.'.$ext).$width.'x'.$height.'.'.$ext;
+      $image->save($path.basename($filename, '.'.$ext).$width.'x'.$height.'.'.$ext);
+    }
+    $this->path = json_encode($paths);
 	}
 }
