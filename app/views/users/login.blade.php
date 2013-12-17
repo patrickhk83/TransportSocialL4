46940ac46c6a2132c1fe7baea999459763afb3cf
ddksaku <ddksaku@gmail.com>
2013-12-17 17:42:10 +0800

    Added "Forgot password" button and infomation view on login view.

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
 @section('content')
 	@include('_partials.errors')
+	@include('_partials.infos')
 	{{ Form::open(array('route' => 'users.auth', 'id' => 'login_form'))}}
 	<div class="form-group">
 		<label for="email">Email</label>
@@ -12,6 +13,6 @@
 	</div>
 	{{ Form::submit('Login', array('class' => 'btn btn-primary')) }}
 	{{ link_to_route('users.register', 'Register', null, array('class' => 'btn btn-primary')) }}
-
+	{{ link_to_route('users.resetpasswordForm', trans('user_auth.link_to_reset_password') , null, array('class' => 'btn btn-primary')) }}
 	{{ Form::close() }}
 @stop
